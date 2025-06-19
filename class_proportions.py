e469ebfe81d1d997b5264c1c9e9162da4c24c840
Benji Sanford Blades <b.sanfordblades@gmail.com>
2025-06-19 14:15:43 -0700

    script to calculate class proportions

---
+++ class_proportions.py
@@ -0,0 +1,30 @@
+from glob import glob
+import os
+
+import laspy
+import numpy as np
+
+def class_proportions(
+        input_dir: str, ignored_classes: list[int]) -> dict[int, float]:
+    counts = {}
+    total = 0
+    for fp in glob(os.path.join(input_dir, "*.la[sz]")):
+        print(f"Processing {os.path.basename(fp)}")
+        las = laspy.read(fp)
+        for label in np.unique(las.classification):
+            if label in ignored_classes:
+                continue
+            count = np.sum(las.classification == label)
+            if label in counts.keys():
+                counts[label] += count
+            else:
+                counts[label] = count
+            total += count
+
+    return {c: counts[c] / total for c in counts.keys()}
+
+if __name__ == "__main__":
+    input_dir = r"Z:\LOUISE_ETHIER\from_Olena\data_for_KPConv\training_small"
+    ignored_classes = [7, 12]
+
+    print(class_proportions(input_dir, ignored_classes))
